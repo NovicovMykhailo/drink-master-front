0d992e14cf1b4770fa20e29c6b4e53156b2f1423
NovicovMykhailo <mikenovicov@gmail.com>
2023-12-16 17:40:35 +0200

    i hope it will work on ghpages

--- src/redux/Drinks/DrinksOperation.js
+++ src/redux/Drinks/DrinksOperation.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-
+import { filteredQuery } from '../../utils/utils'
 import axios from 'axios';
 import setAuthHeader from 'helpers/axiosHedder';
 
@@ -8,17 +8,33 @@ axios.defaults.baseURL = 'https://drink-master-back-end.onrender.com/';
 
 export const fetchDrinks = createAsyncThunk(
   'drinks/fetchDrinks',
-  async ({category = '', ingredient = '', q = '', page = 1}, { getState, rejectWithValue }) => {
+
+  async ({ category, ingredient, q, page}, { getState, rejectWithValue }) => {
     const state = getState();
     const persistedToken = state.auth.token;
+
+    const query = {
+      ingrdient: ingredient ? ingredient : null,
+      category: category ? category : null,
+      q: q ? q : null,
+      page: page > 1 ? page : 1,
+    };
+
+    const normanizedQuery = filteredQuery(query)
+    const params = new URLSearchParams(normanizedQuery);
+
     try {
       setAuthHeader(persistedToken);
-      const response = await axios.get(`api/search?q=${q}&category=${category}&ingredient=${ingredient}&page=${page}`);
+
+      const response = await axios.get(`api/search?${params}`);
       return response.data;
     } catch (error) {
       return rejectWithValue(error.message);
     }
   }
+
+
+
 );
 
 export const fetchCategories = createAsyncThunk('drinks/fetchCategories', async (_, { getState, rejectWithValue }) => {
